6b920d31de0b99324c1b1236f590d41cc9455c63
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-10-28 09:48:43 +0530

    Video post form

--- video face picker/index.js
+++ video face picker/index.js
@@ -1,5 +1,14 @@
-$(document).on("change", ".file_multi_video", function(evt) {
-    var $pre = $('#video_here');
-    $pre[0].src = URL.createObjectURL(this.files[0]);
-    $pre.parent()[0].load();
-  });
+$(document).on("change", ".file_multi_video", function (evt) {
+  var $pre = $('#video_here');
+  $pre[0].src = URL.createObjectURL(this.files[0]);
+  $pre.parent()[0].load();
+});
+
+var text;
+document.getElementById('button').addEventListener('click', function () { var files = document.getElementById('file').files; if (files.length > 0) { getBase64(files[0]); } }); var text; function getBase64(file) {
+  var reader = new FileReader(); reader.readAsDataURL(file); reader.onload = function () {
+    text = reader.result;
+
+  };
+  reader.onerror = function (error) { console.log('Error: ', error); };
+} 
